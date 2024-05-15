969e90215cb4684c33e9c2e7521acabad5e21d63
Anmol <anmol.patankar@coditas.com>
2024-05-15 09:50:05 +0530

    stable, before changing db and controllers, is morning

---
+++ RSMS/Controllers/CommonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RSMS.ActionAttributes;
+using RSMS.Services;
+using RSMS.ViewModels;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RSMS.Controllers
+{
+    public class CommonController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult GetStores()
+        {
+            return View(DatabaseService.GetAllStores());
+        }
+        //public IActionResult GetTransactions()
+        //{
+
+        //}
+        //public IActionResult GetProducts()
+        //{
+
+        //}
+    }
+}
